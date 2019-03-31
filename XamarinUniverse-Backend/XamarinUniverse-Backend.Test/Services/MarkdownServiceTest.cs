cb36d469a4fb23211d72bb1a137113c1aa212f0a
Erick Concepcion <erickconcepcion@live.com>
2019-03-30 23:33:01 -0400

    Adding unit test and deleting build result folders.

---
+++ XamarinUniverse-Backend/XamarinUniverse-Backend.Test/Services/MarkdownServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XamarinUniverse_Backend.Test.Services
+{
+    public class MarkdownServiceTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+    }
+}
